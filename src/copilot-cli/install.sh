81a981a2a40d71df5d46bec3c20c845d088ef7d0
John Keech <jokeech@microsoft.com>
2023-03-06 05:01:57 +0000

    Remove auth

--- src/copilot-cli/install.sh
+++ src/copilot-cli/install.sh
@@ -7,8 +7,4 @@ npm install -g @githubnext/github-copilot-cli
 echo Setting up bash aliases...
 github-copilot-cli alias -- /bin/bash >> $_REMOTE_USER_HOME/.bashrc
 
-echo Setting up auth...
-echo -n $GITHUB_TOKEN > $_REMOTE_USER_HOME/.copilot-cli-access-token
-COPILOT_TOKEN=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/copilot_internal/v2/token | jq -r .token)
-echo -n $COPILOT_TOKEN > $_REMOTE_USER_HOME/.copilot-cli-copilot-token
 echo Done!
